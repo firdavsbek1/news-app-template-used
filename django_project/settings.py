cd905413a200bccc2e33352f5eff00fd5b03a9b4
firdavsbek1 <jalolov.firdavs.official@gmial.com>
2023-07-21 17:23:43 +0500

    last change

--- django_project/settings.py
+++ django_project/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str("NAME"),
-        'USER':env.str("USER"),
-        'PASSWORD':env.str("PASSWORD"),
-        'HOST':env.str("HOST"),
-        'PORT':5432
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env.str("NAME"),
+#         'USER':env.str("USER"),
+#         'PASSWORD':env.str("PASSWORD"),
+#         'HOST':env.str("HOST"),
+#         'PORT':5432
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
